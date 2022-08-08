a6f8b2162b6f24c80da84020b7fb7d2b2a00a0f8
ForbesSolis <abayomiomotayoforbes@gmail.com>
2022-08-08 14:00:52 +0100

    update

---
+++ main.js
@@ -0,0 +1,9 @@
+// document.getElementById('tomatodiv')
+
+var seconddiv = document.getElementById('tomatodiv')
+
+seconddiv.addEventListener('click' , function () {
+    alert ('tomato div is clicked')
+})
+
+seconddiv.style.background = "blue"
